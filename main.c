09aa7e68b1d962d6ec90e8e41bc702e43368bfad
asd-35 <saridoganarda35@gmail.com>
2021-05-19 16:44:38 +0300

    init

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <semaphore.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/time.h>
+
+
+int main()
+{
+
+    return 0;
+}
